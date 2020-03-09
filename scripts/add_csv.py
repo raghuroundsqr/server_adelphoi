a29c3a5f23b37becf8e0965dbc5ceff0903a9895
raghuroundsqr <raghunandan.kasula@roundsqr.net>
2020-03-09 10:47:41 +0530

    Updated script for CSV file insertion
    Version1 for data insertion from CSV file to MongoDB.

--- scripts/add_csv.py
+++ scripts/add_csv.py
@@ -1,6 +1,104 @@
 import csv
+import os
+import pandas as pd
 from server_adelphoi.FirstMatch.models import ModelTests
+import datetime
 def run():
+    df = pd.read_excel('C:/Users/Raghu/Downloads/saturday_7_0.xlsx')
+    df_obj = df.select_dtypes(include=["object"])
+    print(df_obj.columns)
+    for col in df_obj.columns:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+    df_1=df[df['Gender']==1]
+    values = {'Family support': 1,
+              'Level of aggression': 2,
+              'Fire setting': 0,
+              'Client self-harm': 0,
+              'Abuse, or neglect': 1,
+              'CANS_LifeFunctioning': 13,
+              'CANS_YouthStrengths': 14,
+              'CANS_CareGiverStrengths': 10,
+              'CANS_Culture': 0,
+              'CANS_YouthBehavior': 9,
+              'CANS_YouthRisk': 4,
+              'CANS_Trauma_Exp': 5,
+              'FAST_FamilyTogetherScore': 8,
+              'FAST_CaregiverAdvocacyScore': 7,
+              'YLS_PriorCurrentOffenses_Score': 1,
+              'YLS_FamCircumstances_Score': 4,
+              'YLS_Edu_Employ_Score': 3,
+              'YLS_Peer_Score': 3,
+              'YLS_Subab_Score': 2,
+              'YLS_Leisure_Score': 2,
+              'YLS_Personality_Score': 4,
+              'YLS_Attitude_Score': 10,
+              'Screening tool for Trauma--Total score': 15}
+    df_1 = df_1.fillna(value=values)
+    df_2 = df[df['Gender'] == 2]
+    value = {'Family support': 1,
+             'Level of aggression': 2,
+             'Fire setting': 0,
+             'Client self-harm': 0,
+             'Abuse, or neglect': 1,
+             'CANS_LifeFunctioning': 11,
+             'CANS_YouthStrengths': 13,
+             'CANS_CareGiverStrengths': 7,
+             'CANS_Culture': 0,
+             'CANS_YouthBehavior': 8,
+             'CANS_YouthRisk': 4,
+             'CANS_Trauma_Exp': 4,
+             'FAST_FamilyTogetherScore': 7,
+             'FAST_CaregiverAdvocacyScore': 6,
+             'YLS_PriorCurrentOffenses_Score': 1,
+             'YLS_FamCircumstances_Score': 3,
+             'YLS_Edu_Employ_Score': 2,
+             'YLS_Peer_Score': 2,
+             'YLS_Subab_Score': 1,
+             'YLS_Leisure_Score': 2,
+             'YLS_Personality_Score': 3,
+             'YLS_Attitude_Score': 1,
+             'Screening tool for Trauma--Total score': 15}
+    df_2 = df_2.fillna(value=value)
+    df = pd.concat([df_1, df_2])
+    df.fillna(0,inplace=True)
+    numeric_cols = ['PrimaryRacecode', 'SecondaryRaceCode', 'PrimaryLanguage', 'CYF_code',
+                    'EnrollmentStatus', 'Level_of_Care', 'RefSourceCode', 'ClientDischarge',
+                    'ReturntoCare_c',
+                    'Number of prior treatment terminations (excluding shelter or detention)',
+                    'Length of time since living at home', 'Termination directly to AV',
+                    'Death Caregiver',
+                    'Death Silblings', 'Alcohol Use', 'English as the second language',
+                    'Incarcerated caregivers', 'Incarcerated siblings',
+                    'Number of prior AWOLS', 'Animal cruelty', 'Hist of prior program SAO',
+                    'Number of prior hospitalizations', 'Compliant with medication',
+                    'Significant mental health symptoms', 'Severe mental health symptoms',
+                    'Autism Diagnosis', 'Borderline Personality', 'Psychosis',
+                    'Reactive Attachment Disorder', 'Schizophrenia', 'Family support', 'Level of aggression',
+                    'Fire setting',
+                    'Client self-harm', 'Abuse, or neglect', 'CANS_LifeFunctioning',
+                    'CANS_YouthStrengths', 'CANS_CareGiverStrengths', 'CANS_Culture',
+                    'CANS_YouthBehavior', 'CANS_YouthRisk', 'CANS_Trauma_Exp',
+                    'FAST_FamilyTogetherScore', 'FAST_CaregiverAdvocacyScore',
+                    'YLS_PriorCurrentOffenses_Score', 'YLS_FamCircumstances_Score',
+                    'YLS_Edu_Employ_Score', 'YLS_Peer_Score', 'YLS_Subab_Score',
+                    'YLS_Leisure_Score', 'YLS_Personality_Score', 'YLS_Attitude_Score',
+                    'Screening tool for Trauma--Total score']
+    for col in numeric_cols:
+        df[col] = df[col].fillna(0).astype(int)
+
+    df_flt11 = df.select_dtypes(include=["float"])
+    df[['Type of drugs listed','ClientDx', 'Level of care of previous termination']]=df[['Type of drugs listed','ClientDx', 'Level of care of previous termination']].astype(str)
+    dir = r'D:\Adelphoi\AdelphoiProject\\'
+    try:
+        os.makedirs(dir+'Csv_files/'+'/')
+        print("came to directory")
+    except:
+        pass
+    reportfilename = str(str(datetime.datetime.now().strftime('%Y-%m-%d')) + ".csv")
+
+    csv_file_path = dir + 'Csv_files' + '/' + reportfilename
+    df.to_csv(csv_file_path)
+
     header2 = ['ClientCode', 'DoB', 'EpisodeStart', 'EpisodeEnd', 'EpisodeNumber',
        'AgeAtEpisodeStart', 'AgeAtEpisodeEnd', 'EpisodeLengthYrs',
        'EpisodeLengthMo', 'Gender', 'PrimaryRacecode', 'SecondaryRaceCode',
@@ -32,7 +130,7 @@ def run():
        'Autism Diagnosis', 'Borderline Personality', 'Psychosis',
        'Reactive Attachment Disorder', 'Schizophrenia']
 
-    csvfile = open(r'C:/Users/Raghu/Downloads/698_Raw_csv.csv', 'r')
+    csvfile = open(csv_file_path, 'r')
     reader = csv.DictReader(csvfile)
     for each in reader:
         row = {}
